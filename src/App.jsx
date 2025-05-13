af401aa0ce7a4489cff4a6237bbd5facd9d4539c
Krutik Naina <nenakrutik2004@gmail.com>
2025-05-14 00:40:58 +0530

    Analytics Add in website

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Footer from "./components/Footer";
 import SkillsSection from "./components/SkillsSection";
 import ContactSection from "./components/ContactSection";
 import EducationSection from "./components/EducationSection";
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
     <>
+    <div>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
@@ -19,6 +21,8 @@ const App = () => {
         <EducationSection />
         <ContactSection />
         <Footer />
+      </div>
+      <Analytics />
       </div>
     </>
   );
