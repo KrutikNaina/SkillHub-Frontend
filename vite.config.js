1fe592f12ecc51e389e2e14126fe1e248af0ad74
Krutik Naina <nenakrutik2004@gmail.com>
2025-09-20 18:58:43 +0530

    revoke code some testing

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: './', // ✅ Important for production build
-
-  build: {
-    // ✅ Output build directly into backend's dist folder
-    outDir: path.resolve(__dirname, '../skillhub-backend/dist'),
-    emptyOutDir: true, // clears previous build
-  },
 
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
     },
   },
-});
+  
+})
