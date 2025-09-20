937b6d5260300f415c8d31fcde8a8b632c2dba99
Krutik Naina <nenakrutik2004@gmail.com>
2025-09-20 18:49:02 +0530

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: './', // ✅ Important for production build
+
+  build: {
+    // ✅ Output build directly into backend's dist folder
+    outDir: path.resolve(__dirname, '../skillhub-backend/dist'),
+    emptyOutDir: true, // clears previous build
+  },
 
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
     },
   },
-  
-})
+});
